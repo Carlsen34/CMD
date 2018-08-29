eaea8d2f9369667ac87bfef2c81cff5fae167ee0
Herick Carlsen <herick.carlsen@softvaro.com.br>
2018-08-29 15:07:13 -0300

    Comentario

--- src/LPD.java
+++ src/LPD.java
@@ -19,26 +19,31 @@ public class LPD {
 	
 	public void eliminarComentario() {
 		Stack aux = new Stack();
+		int abre = 0;
+		int fecha = token.size();
 
 		for(int i = 0; i<token.size();i++) {
-			
-			if(token.get(i).equals('{')) {
-
-				do {
-					i++;
-				}while(!token.get(i).equals('}'));
-				
-			i++;
-				
+			if(token.get(i).equals('{') && abre == 0) {
+				abre = i;
 			}
-			
-		
-			
-	
-			aux.add(token.get(i));
-
+			if(token.get(i).equals('}') && fecha > abre ) {
+				fecha = i;
+			}
 		}
 		
+		for(int i = 0; i<token.size();i++) {
+			if(i < abre) {
+				aux.add(token.get(i));
+			}
+			if(i > fecha) {
+				aux.add(token.get(i));
+			}
+		}
+	
+		
+		
+		System.out.println(abre);
+		System.out.println(fecha);
 		System.out.println(aux);
 		
 	}
