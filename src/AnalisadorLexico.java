adb5bc37704a4693b0c4f575236ae61b4a63bf85
Herick Carlsen <herick.carlsen@softvaro.com.br>
2018-08-30 13:41:17 -0300

    ajuste

--- src/AnalisadorLexico.java
+++ src/AnalisadorLexico.java
@@ -2,5 +2,37 @@ import java.util.Stack;
 
 public class AnalisadorLexico {
 	
+
+	public static void eliminarComentario(Stack token) {
+		Stack aux = new Stack();
+		int abre = 0;
+		int fecha = token.size();
+
+		for(int i = 0; i<token.size();i++) {
+			if(token.get(i).equals('{') && abre == 0) {
+				abre = i;
+			}
+			if(token.get(i).equals('}') && fecha > abre ) {
+				fecha = i;
+			}
+		}
+		
+		for(int i = 0; i<token.size();i++) {
+			if(i < abre) {
+				aux.add(token.get(i));
+			}
+			if(i > fecha) {
+				aux.add(token.get(i));
+			}
+		}
+	
+		
+		
+		System.out.println(abre);
+		System.out.println(fecha);
+		System.out.println(aux);
+		
+	}
+	
 	
 }
--- src/LPD.java
+++ src/LPD.java
@@ -6,50 +6,14 @@ import java.util.Stack;
 
 public class LPD {
 	
-	
 	Stack token = new Stack();
 	AnalisadorLexico lexico = new AnalisadorLexico();
 	
 	public LPD() {
 		lerLPDLinha();
-		eliminarComentario();
+		AnalisadorLexico.eliminarComentario(token);
 	}
 		
-
-	
-	public void eliminarComentario() {
-		Stack aux = new Stack();
-		int abre = 0;
-		int fecha = token.size();
-
-		for(int i = 0; i<token.size();i++) {
-			if(token.get(i).equals('{') && abre == 0) {
-				abre = i;
-			}
-			if(token.get(i).equals('}') && fecha > abre ) {
-				fecha = i;
-			}
-		}
-		
-		for(int i = 0; i<token.size();i++) {
-			if(i < abre) {
-				aux.add(token.get(i));
-			}
-			if(i > fecha) {
-				aux.add(token.get(i));
-			}
-		}
-	
-		
-		
-		System.out.println(abre);
-		System.out.println(fecha);
-		System.out.println(aux);
-		
-	}
-	
-	
-	
 
 	public void lerLPDLinha() {
 		
