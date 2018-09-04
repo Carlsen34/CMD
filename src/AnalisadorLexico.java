43df0e8d835b589d6979c4bbe92612e5f9132efa
Herick Carlsen <herick.carlsen@softvaro.com.br>
2018-09-04 14:03:48 -0300

    Analisador Lexico

--- src/AnalisadorLexico.java
+++ src/AnalisadorLexico.java
@@ -3,107 +3,297 @@ import java.util.Stack;
 public class AnalisadorLexico {
 	static Stack token = new Stack();
 
-
 	public static Stack eliminarComentario(Stack caracter) {
 		Stack aux = new Stack();
 
-
-		for(int i = 0; i<caracter.size();i++) {
-			if(caracter.get(i).equals('{')) {
+		for (int i = 0; i < caracter.size(); i++) {
+			if (caracter.get(i).equals('{')) {
 				do {
-					i++;	
-				}while(!caracter.get(i).equals('}'));
-		}else {
-			aux.add(caracter.get(i));
+					i++;
+				} while (!caracter.get(i).equals('}'));
+			} else {
+				aux.add(caracter.get(i));
 
-		}
+			}
 		}
 
-		
 		return aux;
 
-		
 	}
-	
+
 	public static Stack consumirEspaco(Stack caracter) {
 		Stack aux = new Stack();
-		
-		for(int i = 0;i<caracter.size();i++) {
-			if(!caracter.get(i).equals(' ')) {
+
+		for (int i = 0; i < caracter.size(); i++) {
+			if (!caracter.get(i).equals(' ')) {
 				aux.add(caracter.get(i));
 			}
-			
+
 		}
-		
-		
+
 		return aux;
 
 	}
-	
+
 	public static Stack pegaToken(Stack caracter) {
 		Simbulo simbulos = new Simbulo();
-		for(int i = 0; i<caracter.size();i++) {
+		for (int i = 0; i < caracter.size(); i++) {
 			String aux = caracter.get(i).toString();
-			if(isDigit(aux) == false) {
-				i = tratarCaracter(caracter,i);
-			}else {
-				i = tratarDigito(caracter,i);
+			if (isDigit(aux) == false) {
+				i = tratarCaracter(caracter, i);
+			} else {
+				i = tratarDigito(caracter, i);
 			}
 		}
-		
-		
+
 		return token;
 	}
-	
-	
-	
-	
-	private static int tratarDigito(Stack caracter,int i) {
+
+	public static int tratarDigito(Stack caracter, int i) {
 		String aux = "";
 		do {
 			aux = aux + caracter.get(i).toString();
 			i++;
 
-		}while(isDigit(caracter.get(i).toString())==true);
-		
+		} while (isDigit(caracter.get(i).toString()) == true);
+
 		token.add(aux);
-		token.add(Simbulo.Snumero);
-		return i-1;
+		token.add(Simbulo.snumero);
+		return i - 1;
 
 	}
-	
-	
-	private static int tratarCaracter(Stack caracter,int i) {
+
+	public static int tratarCaracter(Stack caracter, int i) {
 		String aux = "";
 		do {
-	
+
 			aux = aux + caracter.get(i).toString();
 			i++;
-
-			if(i == caracter.size()) {
-				System.out.println(aux);
-				return i;
+		
+			Boolean identificador = true;
+			verificarPalavrasReservadas(aux);
+			
+			if(aux.equals("programa")) {
+				token.add(aux);
+				token.add(Simbulo.sprograma);
+				aux = "";
+				identificador = false;
 			}
 			
-		}while(isDigit(caracter.get(i).toString())==false);
+			if(aux.equals("início")) {
+				token.add(aux);
+				token.add(Simbulo.sinício);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("fim")) {
+				token.add(aux);
+				token.add(Simbulo.sfim);
+				aux = "";
+				identificador = false;
+
+			}
+			if(aux.equals("procedimento")) {
+				token.add(aux);
+				token.add(Simbulo.sprocedimento);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("funcao")) {
+				token.add(aux);
+				token.add(Simbulo.sfuncao);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("se")) {
+				token.add(aux);
+				token.add(Simbulo.sse);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("entao")) {
+				token.add(aux);
+				token.add(Simbulo.sentao);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("senao")) {
+				token.add(aux);
+				token.add(Simbulo.ssenao);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("enquanto")) {
+				token.add(aux);
+				token.add(Simbulo.senquanto);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("faca")) {
+				token.add(aux);
+				token.add(Simbulo.sfaca);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("início")) {
+				token.add(aux);
+				token.add(Simbulo.sinício);
+				aux = "";
+				identificador = false;
+
+			}
+	
+			if(aux.equals("fim")) {
+				token.add(aux);
+				token.add(Simbulo.sfim);
+				aux = "";
+				identificador = false;
+
+			}
+	
+			
+			if(aux.equals("escreva")) {
+				token.add(aux);
+				token.add(Simbulo.sescreva);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("leia")) {
+				token.add(aux);
+				token.add(Simbulo.sleia);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("var")) {
+				token.add(aux);
+				token.add(Simbulo.svar);
+				aux = "";
+				identificador = false;
+
+			}
+			
+		
+			if(aux.equals("inteiro")) {
+				token.add(aux);
+				token.add(Simbulo.sinteiro);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("booleano")) {
+				token.add(aux);
+				token.add(Simbulo.sbooleano);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("verdadeiro")) {
+				token.add(aux);
+				token.add(Simbulo.sverdadeiro);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("falso")) {
+				token.add(aux);
+				token.add(Simbulo.sfalso);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("div")) {
+				token.add(aux);
+				token.add(Simbulo.sdiv);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("e")) {
+				token.add(aux);
+				token.add(Simbulo.se);
+				aux = "";
+				identificador = false;
+
+			}
+	
+			if(aux.equals("ou")) {
+				token.add(aux);
+				token.add(Simbulo.sou);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("nao")) {
+				token.add(aux);
+				token.add(Simbulo.snao);
+				aux = "";
+				identificador = false;
+
+			}
+			
+			if(aux.equals("contador")) {
+				token.add(aux);
+				token.add(Simbulo.sidentificador);
+				aux = "";
+				identificador = false;
+
+			}
+			
+//			if(identificador == true) {
+//				token.add(aux);
+//				token.add(Simbulo.sidentificador);
+//				aux = "";
+//				identificador = false;
+//			}
+
+			
+
+		}while(i<caracter.size() && isDigit(caracter.get(i).toString()) == false );
+
 		System.out.println(aux);
 
-		return i-1;
+		return i - 1;
 	}
 	
 	
+	public static void verificarPalavrasReservadas(String aux) {
+		
+	}
+	
 
-	private static boolean isDigit(String aux) {
-		if(aux.equals("0") || aux.equals("1") || aux.equals("2") || aux.equals("3") || aux.equals("4") || aux.equals("5") || aux.equals("6") ||
-				aux.equals("7") || aux.equals("8") || aux.equals("9")) {
-		return true;
+	public static boolean isDigit(String aux) {
+		if (aux.equals("0") || aux.equals("1") || aux.equals("2") || aux.equals("3") || aux.equals("4")
+				|| aux.equals("5") || aux.equals("6") || aux.equals("7") || aux.equals("8") || aux.equals("9")) {
+			return true;
 		}
 		return false;
-	
-}
 
-	
+	}
 
-	
-	
 }
--- src/Simbulo.java
+++ src/Simbulo.java
@@ -18,28 +18,29 @@ public class Simbulo {
 	static final int sleia = 13;
 	static final int svar =14;
 	static final int sinteiro = 15;
-	static final int Sbooleano = 16;
-	static final int Sidentificador = 17;
-	static final int Snumero = 18;
-	static final int Sponto = 19;
+	static final int sbooleano = 16;
+	static final int sidentificador = 17;
+	static final int snumero = 18;
+	static final int sponto = 19;
 	static final int sponto_vírgula = 20;
-	static final int Svírgula = 21;
+	static final int svírgula = 21;
 	static final int sabre_parênteses = 22;
 	static final int sfecha_parênteses = 23;
-	static final int Smaior =24;
-	static final int Smaiorig = 25;
-	static final int Sdif = 26;
-	static final int Smais = 27;
-	static final int Smenos = 28;
-	static final int Smult = 29;
-	static final int Sdiv = 30;
-	static final int Se = 31;
-	static final int Sou = 32;
-	static final int Snao =33;
-	static final int Sdoispontos = 34;
-	static final int Sig = 35;
-	static final int Smenorig = 35;
-
+	static final int smaior =24;
+	static final int smaiorig = 25;
+	static final int sdif = 26;
+	static final int smais = 27;
+	static final int smenos = 28;
+	static final int smult = 29;
+	static final int sdiv = 30;
+	static final int se = 31;
+	static final int sou = 32;
+	static final int snao =33;
+	static final int sdoispontos = 34;
+	static final int sig = 35;
+	static final int smenorig = 35;
+	static final int sverdadeiro = 36;
+	static final int sfalso = 37;
 	
 	
 }
