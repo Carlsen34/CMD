9904b07ad12aa01ea40a208e4b72f27a7b2f0462
Herick Carlsen <herick.carlsen@softvaro.com.br>
2018-09-12 15:06:43 -0300

    ajuste simbulos

--- src/AnalisadorLexico.java
+++ src/AnalisadorLexico.java
@@ -22,15 +22,21 @@ public class AnalisadorLexico {
 
 	public static Stack consumirEspaco(Stack caracter) {
 		Stack aux = new Stack();
-		System.out.println(caracter);
-		
-		for(int i = 0; i<caracter.size();i++) {
-			
-			if(!caracter.get(i).equals(' ')) {
-			aux.add(caracter.get(i));
-			}else {
-				//tratarToken(aux);
-				System.out.println(aux);
+
+		for (int i = 0; i < caracter.size(); i++) {
+
+			if (!caracter.get(i).equals(' ')) {
+				aux.add(caracter.get(i));
+
+				if (caracter.get(i).equals('(') || caracter.get(i).equals(')') || caracter.get(i).equals(';')) {
+
+					tratarToken(aux);
+					aux.clear();
+
+				}
+
+			} else {
+				tratarToken(aux);
 				aux.clear();
 			}
 		}
@@ -38,15 +44,15 @@ public class AnalisadorLexico {
 	}
 
 	public static void tratarToken(Stack caracter) {
+		 System.out.println(caracter);
+		
 		Simbulo simbulos = new Simbulo();
 		String aux = "";
-		
-		for(int i = 0; i< caracter.size();i++) {
+
+		for (int i = 0; i < caracter.size(); i++) {
 			aux = aux + caracter.get(i);
 		}
 
-		System.out.println(aux);
-		
 		tratarCaracter(aux);
 	}
 
@@ -65,244 +71,243 @@ public class AnalisadorLexico {
 
 	public static void tratarCaracter(String palavra) {
 
-		System.out.println(token);
-			Boolean identificador = true;
+		// System.out.println(token);
+		Boolean identificador = true;
 
-			if (palavra.equals("programa")) {
-				token.add(palavra);
-				token.add(Simbulo.sprograma);
-				identificador = false;
-			}
+		if (palavra.equals("programa")) {
+			token.add(palavra);
+			token.add(Simbulo.sprograma);
+			identificador = false;
+		}
 
-			if (palavra.equals("início")) {
-				token.add(palavra);
-				token.add(Simbulo.sinício);
-				identificador = false;
+		if (palavra.equals("início")) {
+			token.add(palavra);
+			token.add(Simbulo.sinício);
+			identificador = false;
 
-			}
+		}
 
-			if (palavra.equals("fim")) {
-				token.add(palavra);
-				token.add(Simbulo.sfim);
-				identificador = false;
+		if (palavra.equals("fim")) {
+			token.add(palavra);
+			token.add(Simbulo.sfim);
+			identificador = false;
 
-			}
-			if (palavra.equals("procedimento")) {
-				token.add(palavra);
-				token.add(Simbulo.sprocedimento);
-				identificador = false;
+		}
+		if (palavra.equals("procedimento")) {
+			token.add(palavra);
+			token.add(Simbulo.sprocedimento);
+			identificador = false;
 
-			}
+		}
 
-			if (palavra.equals("funcao")) {
-				token.add(palavra);
-				token.add(Simbulo.sfuncao);
-				identificador = false;
+		if (palavra.equals("funcao")) {
+			token.add(palavra);
+			token.add(Simbulo.sfuncao);
+			identificador = false;
 
-			}
+		}
 
-			if (palavra.equals("se")) {
-				token.add(palavra);
-				token.add(Simbulo.sse);
-				identificador = false;
-			}
+		if (palavra.equals("se")) {
+			token.add(palavra);
+			token.add(Simbulo.sse);
+			identificador = false;
+		}
 
-			if (palavra.equals("entao")) {
-				token.add(palavra);
-				token.add(Simbulo.sentao);
-				identificador = false;
-			}
+		if (palavra.equals("entao")) {
+			token.add(palavra);
+			token.add(Simbulo.sentao);
+			identificador = false;
+		}
 
-			if (palavra.equals("senao")) {
-				token.add(palavra);
-				token.add(Simbulo.ssenao);
-				identificador = false;
-			}
+		if (palavra.equals("senao")) {
+			token.add(palavra);
+			token.add(Simbulo.ssenao);
+			identificador = false;
+		}
 
-			if (palavra.equals("enquanto")) {
-				token.add(palavra);
-				token.add(Simbulo.senquanto);
-				identificador = false;
-			}
+		if (palavra.equals("enquanto")) {
+			token.add(palavra);
+			token.add(Simbulo.senquanto);
+			identificador = false;
+		}
 
-			if (palavra.equals("faca")) {
-				token.add(palavra);
-				token.add(Simbulo.sfaca);
-				identificador = false;
-			}
+		if (palavra.equals("faca")) {
+			token.add(palavra);
+			token.add(Simbulo.sfaca);
+			identificador = false;
+		}
 
-			if (palavra.equals("início")) {
-				token.add(palavra);
-				token.add(Simbulo.sinício);
-				identificador = false;
-			}
+		if (palavra.equals("início")) {
+			token.add(palavra);
+			token.add(Simbulo.sinício);
+			identificador = false;
+		}
 
-			if (palavra.equals("fim")) {
-				token.add(palavra);
-				token.add(Simbulo.sfim);
-				identificador = false;
-			}
+		if (palavra.equals("fim")) {
+			token.add(palavra);
+			token.add(Simbulo.sfim);
+			identificador = false;
+		}
 
-			if (palavra.equals("escreva")) {
-				token.add(palavra);
-				token.add(Simbulo.sescreva);
-				identificador = false;
-			}
+		if (palavra.equals("escreva")) {
+			token.add(palavra);
+			token.add(Simbulo.sescreva);
+			identificador = false;
+		}
 
-			if (palavra.equals("leia")) {
-				token.add(palavra);
-				token.add(Simbulo.sleia);
-				identificador = false;
-			}
+		if (palavra.equals("leia")) {
+			token.add(palavra);
+			token.add(Simbulo.sleia);
+			identificador = false;
+		}
 
-			if (palavra.equals("var")) {
-				token.add(palavra);
-				token.add(Simbulo.svar);
-				identificador = false;
-			}
+		if (palavra.equals("var")) {
+			token.add(palavra);
+			token.add(Simbulo.svar);
+			identificador = false;
+		}
 
-			if (palavra.equals("inteiro")) {
-				token.add(palavra);
-				token.add(Simbulo.sinteiro);
-				identificador = false;
-			}
+		if (palavra.equals("inteiro")) {
+			token.add(palavra);
+			token.add(Simbulo.sinteiro);
+			identificador = false;
+		}
 
-			if (palavra.equals("booleano")) {
-				token.add(palavra);
-				token.add(Simbulo.sbooleano);
-				identificador = false;
-			}
+		if (palavra.equals("booleano")) {
+			token.add(palavra);
+			token.add(Simbulo.sbooleano);
+			identificador = false;
+		}
 
-			if (palavra.equals("verdadeiro")) {
-				token.add(palavra);
-				token.add(Simbulo.sverdadeiro);
-				identificador = false;
-			}
+		if (palavra.equals("verdadeiro")) {
+			token.add(palavra);
+			token.add(Simbulo.sverdadeiro);
+			identificador = false;
+		}
 
-			if (palavra.equals("falso")) {
-				token.add(palavra);
-				token.add(Simbulo.sfalso);
-				identificador = false;
-			}
+		if (palavra.equals("falso")) {
+			token.add(palavra);
+			token.add(Simbulo.sfalso);
+			identificador = false;
+		}
 
-			if (palavra.equals("div")) {
-				token.add(palavra);
-				token.add(Simbulo.sdiv);
-				identificador = false;
-			}
+		if (palavra.equals("div")) {
+			token.add(palavra);
+			token.add(Simbulo.sdiv);
+			identificador = false;
+		}
 
-			if (palavra.equals("e")) {
-				token.add(palavra);
-				token.add(Simbulo.se);
-				identificador = false;
-			}
+		if (palavra.equals("e")) {
+			token.add(palavra);
+			token.add(Simbulo.se);
+			identificador = false;
+		}
 
-			if (palavra.equals("ou")) {
-				token.add(palavra);
-				token.add(Simbulo.sou);
-				identificador = false;
-			}
+		if (palavra.equals("ou")) {
+			token.add(palavra);
+			token.add(Simbulo.sou);
+			identificador = false;
+		}
 
-			if (palavra.equals("nao")) {
-				token.add(palavra);
-				token.add(Simbulo.snao);
-				identificador = false;
-			}
+		if (palavra.equals("nao")) {
+			token.add(palavra);
+			token.add(Simbulo.snao);
+			identificador = false;
+		}
 
-			if (palavra.equals("contador")) {
-				token.add(palavra);
-				token.add(Simbulo.sidentificador);
-				identificador = false;
-			}
-			if (palavra.equals(".")) {
-				token.add(palavra);
-				token.add(Simbulo.sponto);
-				identificador = false;
-			}
-			if (palavra.equals(";")) {
-				token.add(palavra);
-				token.add(Simbulo.sponto_vírgula);
-				identificador = false;
-			}
-			if (palavra.equals("(")) {
-				token.add(palavra);
-				token.add(Simbulo.sabre_parênteses);
-				identificador = false;
-			}
-			if (palavra.equals(")")) {
-				token.add(palavra);
-				token.add(Simbulo.sfecha_parênteses);
-				identificador = false;
-			}
-			if (palavra.equals(">")) {
-				token.add(palavra);
-				token.add(Simbulo.smaior);
-				identificador = false;
-			}
-			if (palavra.equals(">=")) {
-				token.add(palavra);
-				token.add(Simbulo.smaiorig);
-				identificador = false;
-			}
-			if (palavra.equals("=")) {
-				token.add(palavra);
-				token.add(Simbulo.sig);
-				identificador = false;
-			}
-			if (palavra.equals("=")) {
-				token.add(palavra);
-				token.add(Simbulo.sig);
-				identificador = false;
-			}
-			if (palavra.equals("<")) {
-				token.add(palavra);
-				token.add(Simbulo.smenor);
-				identificador = false;
-			}
-			if (palavra.equals("<=")) {
-				token.add(palavra);
-				token.add(Simbulo.smenorig);
-				identificador = false;
-			}
-			if (palavra.equals("!=")) {
-				token.add(palavra);
-				token.add(Simbulo.sdif);
-				identificador = false;
-			}
-			if (palavra.equals("+")) {
-				token.add(palavra);
-				token.add(Simbulo.smais);
-				identificador = false;
-			}
-			if (palavra.equals("-")) {
-				token.add(palavra);
-				token.add(Simbulo.smenos);
-				identificador = false;
-			}
-			if (palavra.equals("-")) {
-				token.add(palavra);
-				token.add(Simbulo.smenos);
-				identificador = false;
-			}
-			if (palavra.equals("*")) {
-				token.add(palavra);
-				token.add(Simbulo.smult);
-				identificador = false;
-			}
-			if (palavra.equals("contador")) {
-				token.add(palavra);
-				token.add(Simbulo.sidentificador);
-				identificador = false;
-			}
+		if (palavra.equals("contador")) {
+			token.add(palavra);
+			token.add(Simbulo.sidentificador);
+			identificador = false;
+		}
+		if (palavra.equals(".")) {
+			token.add(palavra);
+			token.add(Simbulo.sponto);
+			identificador = false;
+		}
+		if (palavra.equals(";")) {
+			token.add(palavra);
+			token.add(Simbulo.sponto_vírgula);
+			identificador = false;
+		}
+		if (palavra.equals("(")) {
+			token.add(palavra);
+			token.add(Simbulo.sabre_parênteses);
+			identificador = false;
+		}
+		if (palavra.equals(")")) {
+			token.add(palavra);
+			token.add(Simbulo.sfecha_parênteses);
+			identificador = false;
+		}
+		if (palavra.equals(">")) {
+			token.add(palavra);
+			token.add(Simbulo.smaior);
+			identificador = false;
+		}
+		if (palavra.equals(">=")) {
+			token.add(palavra);
+			token.add(Simbulo.smaiorig);
+			identificador = false;
+		}
+		if (palavra.equals("=")) {
+			token.add(palavra);
+			token.add(Simbulo.sig);
+			identificador = false;
+		}
+		if (palavra.equals("=")) {
+			token.add(palavra);
+			token.add(Simbulo.sig);
+			identificador = false;
+		}
+		if (palavra.equals("<")) {
+			token.add(palavra);
+			token.add(Simbulo.smenor);
+			identificador = false;
+		}
+		if (palavra.equals("<=")) {
+			token.add(palavra);
+			token.add(Simbulo.smenorig);
+			identificador = false;
+		}
+		if (palavra.equals("!=")) {
+			token.add(palavra);
+			token.add(Simbulo.sdif);
+			identificador = false;
+		}
+		if (palavra.equals("+")) {
+			token.add(palavra);
+			token.add(Simbulo.smais);
+			identificador = false;
+		}
+		if (palavra.equals("-")) {
+			token.add(palavra);
+			token.add(Simbulo.smenos);
+			identificador = false;
+		}
+		if (palavra.equals("-")) {
+			token.add(palavra);
+			token.add(Simbulo.smenos);
+			identificador = false;
+		}
+		if (palavra.equals("*")) {
+			token.add(palavra);
+			token.add(Simbulo.smult);
+			identificador = false;
+		}
+		if (palavra.equals("contador")) {
+			token.add(palavra);
+			token.add(Simbulo.sidentificador);
+			identificador = false;
+		}
 
-			// if(identificador == true) {
-			// token.add(Simbulo.sidentificador);
-			// aux = "";
-			// identificador = false;
-			// }
+		// if(identificador == true) {
+		// token.add(Simbulo.sidentificador);
+		// aux = "";
+		// identificador = false;
+		// }
 
 	}
-
 
 	public static boolean isDigit(String aux) {
 		if (aux.equals("0") || aux.equals("1") || aux.equals("2") || aux.equals("3") || aux.equals("4")
