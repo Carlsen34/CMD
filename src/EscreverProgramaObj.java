4c328c037288653f30dd738993500bfd8ee0c750
Carlsen <herick.carlsen@gmail.com>
2018-12-03 18:37:41 -0200

    Ajustes

--- src/EscreverProgramaObj.java
+++ src/EscreverProgramaObj.java
@@ -9,7 +9,7 @@ public class EscreverProgramaObj {
 		BufferedWriter writer = null;
 		try {
 			writer = new BufferedWriter(new FileWriter(
-					"/home/softvaro/Área de Trabalho/Compilador/Compilador/Virtual-Machine/programaObj.txt"));
+					"/home/carlsen/Desktop/Faculdade/Compiladores/Projetos/Virtual-Machine/programaObj.txt"));
 			writer.write(text);
 
 		} catch (IOException e) {
@@ -28,7 +28,7 @@ public class EscreverProgramaObj {
 		BufferedWriter writer = null;
 		try {
 			writer = new BufferedWriter(new FileWriter(
-					"/home/softvaro/Área de Trabalho/Compilador/Compilador/Virtual-Machine/programaObj.txt"));
+					"/home/carlsen/Desktop/Faculdade/Compiladores/Projetos/Virtual-Machine/programaObj.txt"));
 			writer.write(text);
 
 		} catch (IOException e) {
